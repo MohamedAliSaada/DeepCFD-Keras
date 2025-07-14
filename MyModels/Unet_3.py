786287d1db751c7320fd3a3eccf567af177c850f
Mohamed Ali Saada <mohamedsaada2024@gmail.com>
2025-07-14 03:54:32 +0300

    Update Unet_3.py

--- MyModels/Unet_3.py
+++ MyModels/Unet_3.py
@@ -38,11 +38,11 @@ def Unet(input_shape=(172, 79, 3)):
     # ✅ Pad with value 10: [[2,2] height, [2,3] width]
     x = Lambda(lambda t: tf.pad(
         t,
-        paddings=[[0, 0], [2, 2], [2, 3], [0, 0]],
+        paddings=[[0, 0], [2, 2], [2, 4], [0, 0]],
         mode='CONSTANT',
         constant_values=10
     ))(inputs)
-
+    #(176,88)
     # ----- Encoder Path -----
     x, skip1 = encoder_block(x, 16)  # (88, 44)
     x, skip2 = encoder_block(x, 32)  # (44, 22)
